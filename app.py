395a0914416947639b9260e867ec5d65ee022777
aniket01-aws <60210551+aniket01-aws@users.noreply.github.com>
2020-02-11 11:24:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def view_headers():
+    d = dict(
+        headers=dict(request.headers.items()),
+        origin=request.environ['REMOTE_ADDR']
+    )
+    return jsonify(d)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
